22f368feb56211c078e5841d9af7a1de7caeb44c
Benjamin Young <byoung@bigbluehat.com>
2011-04-12 09:09:25 -0400

    added Apache License text to make it official

--- js/jquery.reformed.js
+++ js/jquery.reformed.js
@@ -2,7 +2,22 @@
  * @copyright: BigBlueHat, 2010
  * @license: Apache Foundation License 2.0
  *
- * reformed is a JSON to HTML Form to JSON conversion utility. The objective is to
+ *  Copyright 2011 BigBlueHat
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ * re:form.ed is a JSON to HTML Form to JSON conversion utility. The objective is to
  * build a simple HTML editor for JSON documents. It's intended use is as a frontend
  * to JSON-based API's such as CouchDB, Facebook, or OpenLibrary.
  *
