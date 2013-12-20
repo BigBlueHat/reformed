aa428e34d10ce8fe8c102dab2b8eeb40c8e209f5
BigBlueHat <byoung@bigbluehat.com>
2013-12-20 14:43:38 -0500

    killing off console.log()s

--- js/reformed.js
+++ js/reformed.js
@@ -13,15 +13,12 @@
   var events = {
       "value": function (value) {
         // got some value.  v is the value. cant be string, int, bool, and null.
-        console.log('value', value);
         DOMinate([recent_obj,
           ["input", {"class": "value", "type": "text", "value": value}]]);
       },
       /*"string": function() {},*/
       "key": function (key) {
         // got a key in an object.
-        console.log('key', key);
-        // append
         // TODO: kill this when we kill .front
         while (recent_obj.className !== 'object') {
           recent_obj = recent_obj.parentNode;
@@ -35,33 +32,27 @@
       },
       "openobject": function (key) {
         // opened an object. key is the first key.
-        console.log('opened object; first key', key);
         recent_obj = DOMinate([recent_obj,
           ["fieldset", {"class": "object"}]]);
         parser.onkey(key);
       },
       "closeobject": function () {
         // closed an object.
-        console.log('closed the object');
         recent_obj = recent_obj.parentNode.parentNode.parentNode;
       },
       "openarray": function () {
         // opened an array.
-        console.log('opened array');
         recent_obj = DOMinate([recent_obj,
           ["fieldset", {"class": "array"}]]);
       },
       "closearray": function () {
         // closed an array.
-        console.log('closed array');
       },
       "error": function (e) {
         // an error happened. e is the error.
-        console.log('error', e);
       },
       "end": function () {
         // parser stream is done, and ready to have more stuff written to it.
-        console.log('all done');
       }/*,
       "ready": function() {}
       */
