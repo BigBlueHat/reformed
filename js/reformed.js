9f146b9f48fa5fb4256ebaaf8fad0fcb28cf4372
BigBlueHat <byoung@bigbluehat.com>
2013-12-20 14:27:00 -0500

    added DOMinate Essential; got tired of DOM

--- js/reformed.js
+++ js/reformed.js
@@ -2,62 +2,58 @@
  * re:form.ed rewritten on clarinet.js
  */
 (function() {
+
+  /*DOMinate essential 1.0.1 by Adrian Sieber (adriansieber.com)*/
+  /** NOTE: added return value of appendChild() operation **/
+  DOMinate=function e(t,n,i,r){function l(e){return document.createElement(e)}for(t[0].big&&(t[0]=l(t[0])),i=1;i<t.length;i++)if(r=t[i],r.big)t[0].innerHTML=r;else if(r.pop)return e(r),t[0].appendChild(r[0]);else for(n in r)t[0].setAttribute(n,r[n])};
+
   var parser = exports.parser();
-  var current_obj = [];
   var recent_obj = null;
 
   var events = {
       "value": function (value) {
         // got some value.  v is the value. cant be string, int, bool, and null.
         console.log('value', value);
-        input = document.createElement('input');
-        input.setAttribute('class', 'value');
-        input.setAttribute('type', 'text');
-        input.setAttribute('value', value);
-        // append
-        recent_obj.appendChild(input);
+        DOMinate([recent_obj,
+          ["input", {"class": "value", "type": "text", "value": value}]]);
       },
       /*"string": function() {},*/
       "key": function (key) {
         // got a key in an object.
         console.log('key', key);
-        div = document.createElement('div');
-        div.setAttribute('class', 'kvp');
-        label = document.createElement('label');
-        label.setAttribute('class', 'key');
-        text = document.createTextNode(key);
-        label.appendChild(text);
-        div.appendChild(label);
         // append
-        recent_obj = current_obj[current_obj.length-1].appendChild(div);
+        // TODO: kill this when we kill .front
+        while (recent_obj.className !== 'object') {
+          recent_obj = recent_obj.parentNode;
+        }
+        recent_obj = DOMinate([recent_obj,
+          ["div", {"class": "kvp"}]]);
+        recent_obj = DOMinate([recent_obj,
+          ["div", {"class": "front"},
+            ["label", {"class": "key"}, key]
+          ]]);
       },
       "openobject": function (key) {
         // opened an object. key is the first key.
-        console.log('opened object; first key', key, current_obj);
-        fieldset = document.createElement('fieldset');
-        fieldset.setAttribute('class', 'object');
-        current_obj.push(fieldset);
+        console.log('opened object; first key', key);
+        recent_obj = DOMinate([recent_obj,
+          ["fieldset", {"class": "object"}]]);
         parser.onkey(key);
       },
       "closeobject": function () {
         // closed an object.
         console.log('closed the object');
-        current_obj[current_obj.length-2].appendChild(current_obj[current_obj.length-1]);
-        current_obj.pop();
+        recent_obj = recent_obj.parentNode.parentNode.parentNode;
       },
       "openarray": function () {
         // opened an array.
         console.log('opened array');
-        fieldset = document.createElement('fieldset');
-        fieldset.setAttribute('class', 'array');
-        recent_obj = current_obj[current_obj.length-1].appendChild(fieldset);
-        current_obj.push(recent_obj);
+        recent_obj = DOMinate([recent_obj,
+          ["fieldset", {"class": "array"}]]);
       },
       "closearray": function () {
         // closed an array.
         console.log('closed array');
-        current_obj[current_obj.length-2].appendChild(current_obj[current_obj.length-1]);
-        current_obj.pop();
       },
       "error": function (e) {
         // an error happened. e is the error.
@@ -77,7 +73,7 @@
   };
 
   this.reformed = function (json, into) {
-    current_obj.push(document.getElementById(into));
+    recent_obj = document.getElementById(into);
     parser.write(json).close();
   }
 })();
